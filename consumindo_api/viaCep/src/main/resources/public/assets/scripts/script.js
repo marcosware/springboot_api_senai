443f2ecfec7d9ebed7ab6f88bc5d126a411dd76d
Marcosware <marcosware.dev@gmail.com>
2025-06-12 11:18:42 -0300

    Insert and POST function added

--- consumindo_api/viaCep/src/main/resources/public/assets/scripts/script.js
+++ consumindo_api/viaCep/src/main/resources/public/assets/scripts/script.js
@@ -20,3 +20,80 @@ async function searchCep() {
 }
 
 document.getElementById('cepRegistro').addEventListener('focusout', searchCep);
+
+
+function GET() {
+    fetch('http://localhost:8080/funcionarios', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+    })
+    .then(response => response.json())
+    .then(data => {
+        insertLine(data);
+    })
+    .catch(error => {
+    console.log(error);
+    });
+}
+
+function POST(nome, email, senha, cep, endereco, numero, bairro, cidade, estado) {
+    fetch('http://localhost:8080/funcionarios', { 
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({"nome":nome,
+                                "email":email,
+                                "senha":senha,
+                                "cep":cep,
+                                "endereco":endereco,
+                                "numero":numero,
+                                "bairro":bairro,
+                                "cidade":cidade,
+                                "estado":estado})
+        })
+        .then(response => response.json())
+        .then(data => {
+          console.log("Resposta da API:", data);
+        })
+        .catch(error => {
+          console.error("Erro ao enviar dados:", error);
+        })
+  }
+
+function insert() {
+      event.preventDefault();
+      const nome = document.getElementById('nomeRegistro').value;
+      const email = document.getElementById('emailRegistro').value;
+      const senha = document.getElementById('senhaRegistro').value;
+      const cep = document.getElementById('cepRegistro').value;
+      const endereco = document.getElementById('enderecoRegistro').value;
+      const numero = document.getElementById('numRegistro').value;
+      const bairro = document.getElementById('bairroRegistro').value;
+      const cidade = document.getElementById('cidadeRegistro').value;
+      const estado = document.getElementById('estadoRegistro').value;
+      if(nome && email && senha && cep && endereco
+        && numero && bairro && cidade && estado) {
+        document.getElementsByClassName('form-control').value = "";
+        POST(nome, email, senha, cep, endereco, numero, bairro, cidade, estado);
+        Swal.fire({
+            icon: 'success',
+            title: 'Sucesso!',
+            text: 'Cadastro realizado com sucesso.',
+            color: 'white',
+            background: '#212529',
+            confirmButtonColor: '#DC3746'
+        });
+    } else {
+        Swal.fire({
+          icon: 'error',
+          title: 'Erro!',
+          text: 'Dados inválidos! Preencha todos os campos com dados válidos.',
+          color: 'white',
+          background: '#212529',
+          confirmButtonColor: '#DC3746'
+        });
+    }
+}
