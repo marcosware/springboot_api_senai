78e59be72a4e26a609d79b92115bdd07157fe2b1
Marcosware <marcosware.dev@gmail.com>
2025-06-12 12:14:20 -0300

    Name column type correction

--- consumindo_api/viaCep/src/db/funcionarios.sql
+++ consumindo_api/viaCep/src/db/funcionarios.sql
@@ -5,9 +5,9 @@ Use `rh-tech`;
 DROP TABLE IF EXISTS `funcionarios`;
 
 CREATE TABLE `funcionarios` (
-  `id` bigint(20) NOT NULL UNIQUE AUTO_INCREMENT,
-  `nome` timestamp NOT NULL,
-  `email` varchar(500) NOT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `nome` varchar(500) NOT NULL,
+  `email` varchar(500) UNIQUE NOT NULL,
   `senha` varchar(500) NOT NULL,
   `cep` varchar(500) NOT NULL,
   `endereco` varchar(1000)NOT NULL,
